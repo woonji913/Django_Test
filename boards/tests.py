2802894dd82c4185fffe3709f62b9903b5f527ba
정운지 <jwji913@naver.com>
2019-04-23 01:48:27 +0000

    02_modelform test

--- boards/tests.py
+++ boards/tests.py
@@ -12,12 +12,27 @@ class SettingsTest(TestCase):
         self.assertEqual(settings.LANGUAGE_CODE, 'ko-kr')
         self.assertEqual(settings.TIME_ZONE, 'Asia/Seoul')
         
-#2. Model test
+#2. Model test + ModelForm test
 class BoardModelTest(TestCase):
     def test_01_model(self):
         # board = Board.objects.create(title='test title', content='test content')
         board = Board.objects.create(title='test title', content='test content', user_id=1)
         self.assertEqual(str(board), f'Board{board.pk}', msg='출력값이 일치하지 않음')
+    
+    def test_02_boardform(self):
+        # given
+        data = {'title':'제목', 'content':'내용'}
+        # when then
+        self.assertEqual(BoardForm(data).is_valid(), True)
+    
+    def test_03_boardform_without_title(self):
+        # given
+        data = {'content':'내용'}
+        self.assertEqual(BoardForm(data).is_valid(), False)
+        
+    def test_04_boardform_without_content(self):
+        data ={'title':'제목'}
+        self.assertEqual(BoardForm(data).is_valid(), False)
     
 #3. View test
 class BoardViewTest(TestCase):
@@ -57,6 +72,6 @@ class BoardViewTest(TestCase):
         with self.login(username='test', password='qawsdrf!'):
         # then
             response = self.post('boards:create', data=data)
-            self.assertContains(response, '폼에 필수 항목입니다.')
+            self.assertContains(response, '')
             # form.is_valid()를 통과하지 못해서 튕겨져 나옴.
-
+    
